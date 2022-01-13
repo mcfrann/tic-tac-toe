52c897c810e4ffbc2a34a3051c64bdcf679d7f43
Francesca McConnell <fmcconnell59@gmail.com>
2022-01-13 15:38:04 -0700

    Refactor Player class

--- Player.js
+++ Player.js
@@ -1,7 +1,15 @@
 class Player {
-  constructor(id, token) {
-    this.id = id;
-    this.token = token;
+  constructor(playerId) {
+    this.id = playerId;
+    this.token = '';
     this.wins = [];
+  }
+
+  whichPlayer() {
+    if (this.id === 'one') {
+      this.token = 'assets/image-from-rawpixel-id-2873787-original.png';
+    } else if (this.id === 'two'){
+      this.token = 'assets/image-from-rawpixel-id-2873708-original.png';
+    }
   }
 }
