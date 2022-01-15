7468e28c06f615d10c26f0b6d5000efb30bac324
Francesca McConnell <fmcconnell59@gmail.com>
2022-01-15 11:35:53 -0700

    Push alternating id to gridselection array

--- main.js
+++ main.js
@@ -17,50 +17,23 @@ gridItem.forEach(function(el) {
 // Global variables
 
 
+var game = new Game();
+
 
 // Handlers
 
 
-// function selectGridItem(e) {
-//   gridItem.innerHTML = ``;
-//   for (var i = 0; i < gridItem.length; i++) {
-//     gridItem[i].innerHTML = `hi`;
-//   }
-// }
-
 function switchPlayer(e) {
-  var game = new Game();
-
-  game.currentPlayer.gridSelection.push(e.target.id);
-
-  game.changePlayer();
-
-  console.log(game.gridSelection);
-  console.log(game.currentPlayer);
-
-  switchTurnHeading();
+  if (game.gridSelection.length < 9) {
+     game.gridSelection.push(game.currentPlayer.id);
+     gridItem.innerHTML += `${game.currentPlayer.token}`;
+     game.changePlayer();
+     switchTurnHeading();
+     console.log(game.gridSelection);
+   }
 }
 
 function switchTurnHeading() {
   playerOneTurn.classList.toggle('hidden');
   playerTwoTurn.classList.toggle('hidden');
 }
-// function switchPlayer(e) {
-//   var game = new Game();
-//
-//   if (game.currentPlayer.id === 'one') {
-//     // game.playerOne.gridSelection.push(e.target.id);
-//     game.gridSelection.push(game.currentPlayer.token);
-//     game.changePlayer();
-//   } else if (newGame.currentPlayer.id === 'two') {
-//     game.gridSelection.push(game.currentPlayer.token);
-//     game.changePlayer();
-//     // game.playerTwo.gridSelection.push(e.target.id);
-//   }
-
-  // console.log(game.currentPlayer);
-  // console.log(game.gridSelection);
-  // console.log(game.playerOne.gridSelection);
-  // console.log(game.playerTwo.gridSelection);
-
-// }
