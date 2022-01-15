e46735f26da6ff1dcb56f1d5bed3202e210849e1
Francesca McConnell <fmcconnell59@gmail.com>
2022-01-15 14:21:24 -0700

    Add measure wins, switch between players

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 // Selectors
 
 
+var gridContainer = document.querySelector('.game-board-container');
 var gridItem = document.querySelectorAll('.grid-item');
 var playerOneTurn = document.querySelector('.player-turn-one');
 var playerTwoTurn = document.querySelector('.player-turn-two');
+
 
 
 // Listeners
@@ -24,13 +26,22 @@ var game = new Game();
 
 
 function switchPlayer(e) {
-  if (game.gridSelection.length < 9) {
-     game.gridSelection.push(game.currentPlayer.id);
-     gridItem.innerHTML += `${game.currentPlayer.token}`;
+  game.insertToken();
+  if (game.currentPlayer.gridSelection.length < 5 && game.gridSelection.includes(e.target.id) === false) {
+     game.currentPlayer.gridSelection.push(e.target.id);
+     game.gridSelection.push(e.target.id);
      game.changePlayer();
+     game.gridSectionOne();
+     game.gridSectionTwo();
+     placeToken(e);
      switchTurnHeading();
-     console.log(game.gridSelection);
    }
+}
+
+function placeToken(e) {
+  for (var i = 0; i < gridContainer.length; i++) {
+    if ()
+  }
 }
 
 function switchTurnHeading() {
